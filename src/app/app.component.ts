d6905c76b4b2dbb618b194def6666390523cb3db
JonnasWillian <jonnasnogueira2@gmail.com>
2021-03-20 20:03:04 -0300

    'InicioDoProjeto'

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  listTitles = [{
+    trailer: "assets/videos/trailer_guardioes_da_galaxia_1080p.mp4",
+    cover: "assets/images/guardios-da-galaxia.jpg",
+    name: 'Guardiões da Galaxia',
+    relavance: 98,
+    age: 0,
+    parts: 2,
+    categories: ["Épico", "Fantasia", "Viagem Espacial"]
+  },{
+    trailer: "assets/videos/trailer_guardioes_da_galaxia_1080p.mp4",
+    cover: "assets/images/guardios-da-galaxia.jpg",
+    name: 'Guardiões da Galaxia',
+    relavance: 80,
+    age: 15,
+    parts: 2,
+    categories: ["Épico", "Fantasia", "Viagem Espacial"]
+  },{
+    trailer: "assets/videos/trailer_guardioes_da_galaxia_1080p.mp4",
+    cover: "assets/images/guardios-da-galaxia.jpg",
+    name: 'Guardiões da Galaxia',
+    relavance: 98,
+    age: 12,
+    parts: 2,
+    categories: ["Épico", "Fantasia", "Viagem Espacial"]
+  }
+];
+}
